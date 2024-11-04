17467ec4c6c21ff70e66c354c304015664bf1bbd
Christian Nielsen <120013949+HrSejer@users.noreply.github.com>
2024-11-04 10:20:12 +0100

    Started to setup api key, test to see if gitignore works

--- Avalonia.FilmTvSerieSystem/Program.cs
+++ Avalonia.FilmTvSerieSystem/Program.cs
@@ -1,16 +1,28 @@
 ﻿using Avalonia;
 using System;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace Avalonia.FilmTvSerieSystem
 {
     internal sealed class Program
     {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // Initialization code. Don't use any Avalonia, third-party APIs, or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string apiKey = configuration["TMDb:ApiKey"];
+
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
---
+++ Avalonia.FilmTvSerieSystem/TMDbService.cs
@@ -0,0 +1,22 @@
+﻿using TMDbLib.Client;
+using TMDbLib.Objects.Movies;
+using System.Threading.Tasks;
+
+namespace Avalonia.FilmTvSerieSystem
+{
+    public class TMDbService
+    {
+        private readonly TMDbClient _client;
+
+        public TMDbService(string apiKey)
+        {
+            _client = new TMDbClient(apiKey);
+        }
+
+        public async Task<Movie> GetMovieDetailsAsync(int movieId)
+        {
+            return await _client.GetMovieAsync(movieId);
+        }
+
+    }
+}
